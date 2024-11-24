e5bf190dea13e13ffa385267b4b3f014db6b915d
miguelvelez0824 <mivelezb@unal.edu.co>
2024-11-24 13:23:38 -0500

    correccion en listas

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,25 +3,34 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas= [], estudiantes = []):
+    def __init__(self, grupo="grupo predeterminado", asignaturas= None, estudiantes = None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        if asignaturas is None:
+            self._asignaturas = []
+        else:
+            self._asignaturas = asignaturas
+        if estudiantes is None:
+            self.listadoAlumnos = []
+        else:
+            self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
+    def agregarAlumno(self, alumno, lista= None):
+        if lista is None:
+            lista = []
+        else:
+            lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
         return "Grupo de estudiantes " + self._grupo      
 
-    # @ classmethod
-    # def asignarNombre(cls, nombre="Grado 10"):
-    #     cls.grado = nombre
+    @ classmethod
+    def asignarNombre(cls, nombre="Grado 10"):
+        cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
