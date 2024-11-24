c7be7f9ef61ef091db5ce08cdfdb1e7892c46c04
miguelvelez0824 <mivelezb@unal.edu.co>
2024-11-24 13:57:34 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -21,8 +21,9 @@ class Grupo:
     def agregarAlumno(self, alumno, lista = None):
         if lista is None:
             lista = []
-        self.listadoAlumnos = self.listadoAlumnos + lista
         lista.append(alumno)
+        self.listadoAlumnos = self.listadoAlumnos + lista
+        
 
     def __str__(self):
         return "Grupo de estudiantes " + self._grupo      
